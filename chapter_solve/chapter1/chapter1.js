be3b08b167e343dbbe64e2113ae4352fc4262a77
Zaime Ali <zaimeali1997@gmail.com>
2020-06-02 23:58:54 +0500

    Working

---
+++ chapter_solve/chapter1/chapter1.js
@@ -0,0 +1,27 @@
+function question1(title){
+    window.document.title = title;
+    alert("Welcome to my Home Page");
+}
+
+function question2(title){
+    window.document.title = title;
+    alert("Error! Please enter a valid password.");
+}
+
+function question3(title){
+    window.document.title = title;
+    alert("Welcome to JS Land...\nHappy Coding!");
+}
+
+function question4(title){
+    window.document.title = title;
+    alert("Welcome to JS Land...");
+    alert("Happy Coding!");
+}
+
+function question5(title){
+    window.document.title = title;
+    console.log('Running Alert Through Console');
+    console.log(alert("Hello... I can run JS through my web browser's console"));
+}
+
