03bd54654d066b8ec5aff1f372e3fb4aa416628d
Zaime Ali <zaimeali1997@gmail.com>
2020-06-05 14:48:35 +0500

    Working on Chapter 6-9

---
+++ chapter/chapter6-9/ch69q1.js
@@ -0,0 +1,28 @@
+let a = +prompt("Enter number: ");
+
+document.write("Result:<br>");
+document.write("The value of a is: " + a + "<hr>");
+
+document.write("<br>");
+document.write("The value of ++a is: " + ++a);
+document.write("<br>");
+document.write("Now the value of a is: " + a);
+document.write("<br>");
+
+document.write("<br>");
+document.write("The value of a++ is: " + a++);
+document.write("<br>");
+document.write("Now the value of a is: " + a);
+document.write("<br>");
+
+document.write("<br>");
+document.write("The value of --a is: " + --a);
+document.write("<br>");
+document.write("Now the value of a is: " + a);
+document.write("<br>");
+
+document.write("<br>");
+document.write("The value of a-- is: " + a--);
+document.write("<br>");
+document.write("Now the value of a is: " + a);
+document.write("<br>");
